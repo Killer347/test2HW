3bb8117c946330241f069c57c93878ba9e2260c6
Killer347 <djon_9595@bk.ru>
2020-03-23 15:24:32 +0900

    My first programm pcyharm2

--- mine.py
+++ mine.py
@@ -1,4 +1,8 @@
-import request
-print('HelloWord')
-print('HelloWord')
-print('HelloWord')
+import requests
+def get_btc(): #курс биткоина к доллару
+    url = 'https://yobit.net/api/2/btc_usd/ticker'
+    response = requests.get(url).json()
+    price = response['ticker']['last']
+    return str(price) + ' usd'
+
+print(get_btc())
